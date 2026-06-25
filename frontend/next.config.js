5bf2ab9f9355500a4db3b92ab413902bb0018634
delreyir <delreyir@users.noreply.github.com>
2026-06-25 14:56:58 +0100

    config: remove static export for Vercel deployment, add vercel dep

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: { unoptimized: true },
-  env: { NEXT_PUBLIC_CONTRACT_ADDRESS: "0x4fCBCbF376EBD5b56041A827497773817B5ba32d" },
 };
 module.exports = nextConfig;
